2e6fe13fd982384769bde76d79da4b649c780e98
Lehner Florian <dev@der-flo.net>
2013-10-04 20:40:13 +0200

    improve autogen.sh
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,6 @@ AUTORECONF=`which autoreconf`
 if test -z $AUTORECONF; then
     echo "*** No autoreconf found, please install it ***"
     exit 1
+else
+    autoreconf --force --install --verbose || exit $?
 fi
-
-autoreconf --force --install --verbose || exit $?
