6261ac070faabb63f930dc5dfca22e39c7c1911c
Lehner Florian <dev@der-flo.net>
2014-03-06 19:31:44 +0100

    proper setting of the log
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- src/ContactCards.c
+++ src/ContactCards.c
@@ -6,7 +6,7 @@
 
 #ifdef ContactCards_DEBUG
 static void logHandler(const gchar *domain, GLogLevelFlags level, const gchar *message, gpointer user_data){
-	printf("%s", message);
+	printf("[%s] %s", domain, message);
 }
 #endif  /* ContactCards_DEBUG */
 
@@ -37,7 +37,7 @@ int main(int argc, char **argv){
 	guiInit(db_handler);
 	g_mutex_init(&mutex);
 	#ifdef ContactCards_DEBUG
-	g_log_set_handler(NULL, G_LOG_PROTO, logHandler, NULL);
+	g_log_set_handler("ContactCards", G_LOG_PROTO, logHandler, NULL);
 	#endif
 
 	newOAuthEntity(db_handler, "google.com", "741969998490.apps.googleusercontent.com", "71adV1QbUKszvBV_xXliTD34", "https://www.googleapis.com/.well-known/carddav", "https://www.googleapis.com/auth/carddav", "https://accounts.google.com/o/oauth2/auth", "https://accounts.google.com/o/oauth2/token", "code", "urn:ietf:wg:oauth:2.0:oob", "authorization_code");
