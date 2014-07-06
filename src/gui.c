fc44b386fc58d4745d5415c01d03cbafddb0dc7a
Lehner Florian <dev@der-flo.net>
2014-07-06 14:40:52 +0200

    shorten URLs for the gui
    Signed-off-by: Lehner Florian <dev@der-flo.net>

--- src/gui.c
+++ src/gui.c
@@ -588,7 +588,7 @@ static GtkWidget *buildNewCard(sqlite3 *ptr, int selID){
 				if(value != NULL){
 					char			*uri = NULL;
 					uri = g_strconcat("mailto:", g_strcompress((g_strstrip(value))), NULL);
-					label = gtk_link_button_new_with_label(uri, g_strcompress((g_strstrip(value))));
+					label = gtk_link_button_new_with_label(uri, g_strdup_printf("%.42s", g_strcompress((g_strstrip(value)))));
 					g_free(uri);
 					gtk_widget_set_halign(GTK_WIDGET(label), GTK_ALIGN_START);
 					gtk_grid_attach(GTK_GRID(card), label, 2, line++, 1, 1);
@@ -610,7 +610,8 @@ static GtkWidget *buildNewCard(sqlite3 *ptr, int selID){
 				GSList				*next = list->next;
 				char				*value = list->data;
 				if(value != NULL){
-					label = gtk_link_button_new (g_strcompress((g_strstrip(value))));
+					label = gtk_link_button_new(g_strdup_printf("%.42s", g_strcompress((g_strstrip(value)))));
+					gtk_link_button_set_uri (GTK_LINK_BUTTON(label), g_strcompress((g_strstrip(value))));
 					gtk_widget_set_halign(GTK_WIDGET(label), GTK_ALIGN_START);
 					gtk_grid_attach(GTK_GRID(card), label, 2, line++, 1, 1);
 				}
